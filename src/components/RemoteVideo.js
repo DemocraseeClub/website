54813aaee13844ece6e17209c8b047eab708fded
Branyer Vergara <Branyervergarac@gmail.com>
2021-02-09 17:33:14 -0400

    FIXING PROBLEM #2
    1- The code of the room that was shown on the screen was converted into capital letters, and this did not allow it to be found in the db.
    2- Added a listener that listens when someone connects to the broadcast.

--- src/components/RemoteVideo.js
+++ src/components/RemoteVideo.js
@@ -2,160 +2,215 @@ import React from "react";
 import VideoElement from "./VideoElement";
 import PropTypes from "prop-types";
 import Config from "../Config";
+import { FormatColorResetTwoTone } from "@material-ui/icons";
 
 class RemoteVideo extends React.Component {
+  constructor(p) {
+    super(p);
+    this.state = { remoteStream: p.stream };
+    this.db = p.db;
+    this.view = false;
+  }
 
-    constructor(p) {
-        super(p);
-        this.state = {remoteStream : p.stream};
-        this.db = p.db;
+  componentDidMount() {
+    this.joinRoomById(this.props.roomId);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (!this.props.roomId && prevProps.roomId) {
+      this.hangUp();
     }
+  }
 
-    componentDidMount() {
-        this.joinRoomById(this.props.roomId);
+  async componentWillUnmount() {
+    await this.hangUp();
+  }
+
+  async joinRoomById(room) {
+    if (!this.props.db) {
+      this.db = window.firebase.firestore();
     }
+    const roomRef = this.db.collection("rooms").doc(room);
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (!this.props.roomId && prevProps.roomId) {
-            this.hangUp();
+    // roomRef.collection('rooms').doc(room).get().then(doc => {
+    //     console.log(doc.data());
+    // });
+
+    const roomSnapshot = await roomRef.get();
+
+    console.log("Got room:", roomSnapshot.exists, roomRef);
+    if (!roomSnapshot.exists) return false;
+
+    this.peerConnection = this.createPeerConnection();
+
+    this.peerConnection.onaddstream = (event) => {
+      console.log("ON ADDED STREAM", event);
+      this.setState({ remoteStream: event.stream });
+    };
+
+    // Code for collecting ICE candidates below
+    const calleeCandidatesCollection = roomRef.collection("calleeCandidates");
+    this.peerConnection.addEventListener("icecandidate", (event) => {
+      if (!event.candidate) {
+        console.log("Got final icecandidate!", event);
+        return;
+      }
+      console.log("Got candidate: ", event.candidate);
+      calleeCandidatesCollection.add(event.candidate.toJSON());
+    });
+    // Code for collecting ICE candidates above
+
+    this.peerConnection.addEventListener("track", (event) => {
+      let remoteStream = this.state.remoteStream; // WARN: maybe clone this ?
+      console.log("Got presenter track:", event.streams[0]);
+      event.streams[0].getTracks().forEach((track) => {
+        console.log("Add a track to the remoteStream:", track);
+        remoteStream.addTrack(track);
+      });
+      this.setState({ remoteStream: remoteStream });
+    });
+
+    this.peerConnection.ontrack = (event) => {
+      let remoteStream = this.state.remoteStream; // WARN: maybe clone this ?
+      console.log("Got presenter track:", event.streams[0]);
+      event.streams[0].getTracks().forEach((track) => {
+        console.log("Add a track to the remoteStream:", track);
+        remoteStream.addTrack(track);
+      });
+      this.setState({ remoteStream: remoteStream });
+    };
+
+    // Code for creating SDP answer below
+    const offer = roomSnapshot.data().offer;
+    console.log("Got offer:", offer);
+    await this.peerConnection.setRemoteDescription(
+      new RTCSessionDescription(offer)
+    );
+    const answer = await this.peerConnection.createAnswer();
+    console.log("Created answer:", answer);
+    await this.peerConnection.setLocalDescription(answer);
+
+    const roomWithAnswer = {
+      answer: { type: answer.type, sdp: answer.sdp },
+    };
+    await roomRef.update(roomWithAnswer);
+    // Code for creating SDP answer above
+
+    // Listening for remote ICE candidates below
+    roomRef.collection("callerCandidates").onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach(async (change) => {
+        if (change.type === "added") {
+          let data = change.doc.data();
+          console.log(
+            `Got new remote callerCandidates: ${JSON.stringify(data)}`
+          );
+          await this.peerConnection.addIceCandidate(new RTCIceCandidate(data));
         }
+      });
+    });
+    // Listening for remote ICE candidates above
+  }
+
+  createPeerConnection() {
+    console.log(
+      "Create PeerConnection with configuration: ",
+      Config.peerConfig
+    );
+    let peerConnection = new RTCPeerConnection(Config.peerConfig);
+    peerConnection.addEventListener("icegatheringstatechange", () => {
+      console.log(
+        `ICE gathering state changed: ${peerConnection.iceGatheringState}`
+      );
+    });
+
+    peerConnection.addEventListener("connectionstatechange", async () => {
+      console.log(`Connection state change: ${peerConnection.connectionState}`);
+
+      const roomRef = this.db.collection("rooms").doc(this.props.roomId);
+      const roomSnapshot = await roomRef.get();
+      const viewers = roomSnapshot.data().viewers;
+      switch (peerConnection.connectionState) {
+        case "connected":
+            this.view = true;
+
+
+          roomRef.update({
+            viewers: viewers + 1,
+          });
+
+          break;
+        case "disconnected":
+            this.view = false;
+          console.log("saliendo", viewers)
+            roomRef.update({
+              viewers: viewers -1,
+            });
+          break;
+        default:
+          break;
+      }
+    });
+
+    peerConnection.addEventListener("signalingstatechange", () => {
+      console.log(`Signaling state change: ${peerConnection.signalingState}`);
+    });
+
+    peerConnection.addEventListener("iceconnectionstatechange ", () => {
+      console.log(
+        `ICE connection state change: ${peerConnection.iceConnectionState}`
+      );
+    });
+
+    if (this.state.remoteStream) {
+      console.log("adding remote stream");
+      this.state.remoteStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, this.state.remoteStream);
+      });
     }
 
-    async componentWillUnmount() {
-        await this.hangUp();
+    return peerConnection;
+  }
+
+  async hangUp() {
+    console.log("HANGING UP REMOTE!");
+    if (this.remoteStream) {
+      this.remoteStream.getTracks().forEach((track) => track.stop());
     }
 
-    async joinRoomById(room) {
-        if (!this.props.db) {
-            this.db = window.firebase.firestore();
-        }
-        const roomRef = this.db.collection('rooms').doc(room);
+    if (this.peerConnection) {
+      this.peerConnection.close();
+    }
+
+    if(this.view) {
+        this.view = false;
+
+        const roomRef = this.db.collection("rooms").doc(this.props.roomId);
         const roomSnapshot = await roomRef.get();
-        console.log('Got room:', roomSnapshot.exists, roomRef);
-        if (!roomSnapshot.exists) return false;
+    
+        const viewers = roomSnapshot.data().viewers;
 
-        this.peerConnection = this.createPeerConnection();
-
-        this.peerConnection.onaddstream = (event => {
-            console.log("ON ADDED STREAM", event);
-            this.setState({remoteStream:event.stream})
-        });
-
-        // Code for collecting ICE candidates below
-        const calleeCandidatesCollection = roomRef.collection('calleeCandidates');
-        this.peerConnection.addEventListener('icecandidate', event => {
-            if (!event.candidate) {
-                console.log('Got final icecandidate!', event);
-                return;
-            }
-            console.log('Got candidate: ', event.candidate);
-            calleeCandidatesCollection.add(event.candidate.toJSON());
-        });
-        // Code for collecting ICE candidates above
-
-        this.peerConnection.addEventListener('track', event => {
-            let remoteStream = this.state.remoteStream; // WARN: maybe clone this ?
-            console.log('Got presenter track:', event.streams[0]);
-            event.streams[0].getTracks().forEach(track => {
-                console.log('Add a track to the remoteStream:', track);
-                remoteStream.addTrack(track);
-            });
-            this.setState({remoteStream:remoteStream});
-        });
-
-        this.peerConnection.ontrack = event => {
-            let remoteStream = this.state.remoteStream; // WARN: maybe clone this ?
-            console.log('Got presenter track:', event.streams[0]);
-            event.streams[0].getTracks().forEach(track => {
-                console.log('Add a track to the remoteStream:', track);
-                remoteStream.addTrack(track);
-            });
-            this.setState({remoteStream:remoteStream});
-        }
-
-        // Code for creating SDP answer below
-        const offer = roomSnapshot.data().offer;
-        console.log('Got offer:', offer);
-        await this.peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-        const answer = await this.peerConnection.createAnswer();
-        console.log('Created answer:', answer);
-        await this.peerConnection.setLocalDescription(answer);
-
-        const roomWithAnswer = {
-            answer: {type: answer.type, sdp: answer.sdp}
-        };
-        await roomRef.update(roomWithAnswer);
-        // Code for creating SDP answer above
-
-        // Listening for remote ICE candidates below
-        roomRef.collection('callerCandidates').onSnapshot(snapshot => {
-            snapshot.docChanges().forEach(async change => {
-                if (change.type === 'added') {
-                    let data = change.doc.data();
-                    console.log(`Got new remote callerCandidates: ${JSON.stringify(data)}`);
-                    await this.peerConnection.addIceCandidate(new RTCIceCandidate(data));
-                }
-            });
-        });
-        // Listening for remote ICE candidates above
-
-    }
-
-    createPeerConnection() {
-        console.log('Create PeerConnection with configuration: ', Config.peerConfig);
-        let peerConnection = new RTCPeerConnection(Config.peerConfig);
-        peerConnection.addEventListener('icegatheringstatechange', () => {
-            console.log(`ICE gathering state changed: ${peerConnection.iceGatheringState}`);
-        });
-
-        peerConnection.addEventListener('connectionstatechange', () => {
-            console.log(`Connection state change: ${peerConnection.connectionState}`);
-        });
-
-        peerConnection.addEventListener('signalingstatechange', () => {
-            console.log(`Signaling state change: ${peerConnection.signalingState}`);
-        });
-
-        peerConnection.addEventListener('iceconnectionstatechange ', () => {
-            console.log(`ICE connection state change: ${peerConnection.iceConnectionState}`);
-        });
-
-
-        if (this.state.remoteStream) {
-            console.log("adding remote stream");
-            this.state.remoteStream.getTracks().forEach(track => {
-                peerConnection.addTrack(track, this.state.remoteStream);
-            });
-        }
-
-        return peerConnection;
+        roomRef.update({
+            viewers: viewers - 1 ,
+          });
     }
 
 
-    async hangUp() {
-        console.log("HANGING UP REMOTE!");
-        if (this.remoteStream) {
-            this.remoteStream.getTracks().forEach(track => track.stop());
-        }
+    this.setState({ remoteStream: null });
+  }
 
-        if (this.peerConnection) {
-            this.peerConnection.close();
-        }
-
-        this.setState({remoteStream:null});
-    }
-
-    render() {
-        return (
-            <VideoElement stream={this.state.remoteStream} roomId={this.props.roomId} />
-        );
-    }
-
+  render() {
+    return (
+      <VideoElement
+        stream={this.state.remoteStream}
+        roomId={this.props.roomId}
+        db={this.db}
+      />
+    );
+  }
 }
 
 RemoteVideo.propTypes = {
-    roomId : PropTypes.string.isRequired
+  roomId: PropTypes.string.isRequired,
 };
-
 
 export default RemoteVideo;
--- src/components/VideoElement.js
+++ src/components/VideoElement.js
@@ -6,11 +6,23 @@ class VideoElement extends Component {
 
     constructor(p) {
         super(p);
+        this.listener = false;
         this.vidEl = React.createRef();
-        this.state = {mounted:false, showRoomId:false}
+        this.state = {mounted:false, showRoomId:false, viewers: this.props.viewers}
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("actualizado", this.props)
+
+        if(this.props.roomId && !prevProps.roomId) {
+            this.listener = true;
+            this.props.db.collection('rooms').doc(this.props.roomId).onSnapshot((snap) => {
+                    console.log('snap', snap.data())
+                this.setState({viewers:snap.data().viewers}) ;
+            })
+
+        }
+
         let doUpdate = prevState.mounted === false && this.state.mounted === true;
         if (doUpdate === false) {
             if (this.props.stream.id !== prevProps.stream.id || this.props.roomId !== prevProps.roomId) {
@@ -19,26 +31,35 @@ class VideoElement extends Component {
         }
         if (doUpdate === true ) {
             this.vidEl.current.srcObject = this.props.stream;
+            console.log(this.vidEl.current.srcObject)
             if (this.props.muted === true) {
                 this.vidEl.current.volume = 0;
             }
+
         }
     }
 
-    componentDidMount() {
+    componentDidMount() { 
+        console.log("montado", this.props)
+
+      
+
+
         this.setState({mounted:true})
     }
+
+
 
     render() {
         return (
             <React.Fragment>
                 <video controls style={{height: 250, width: '100%'}} autoPlay ref={this.vidEl} />
                 {(!this.props.roomId) ? '' :
-                    <Typography variant='overline' style={{display:'inline-flex'}} >
-                        {this.props.viewers === -1 ? '' : <span style={{marginRight:5}} >viewers: {this.props.viewers}</span>}
-                        <span style={{marginRight:5}}>{this.state.showRoomId === true ? this.props.roomId : ' **** '}</span>
+                    <p style={{display:'inline-flex', margin:0}} >
+                        {this.props.viewers === -1 ? '' : <span style={{marginRight:5}} >viewers: {this.state.viewers}</span>}
+                        <span style={{marginRight:5, fontWeight:'bold'}}>{this.state.showRoomId === true ? this.props.roomId : ' **** '}</span>
                         <u onClick={() => this.setState({showRoomId:!this.state.showRoomId})}>{this.state.showRoomId === true ? 'hide' : 'show'}</u>
-                    </Typography>
+                    </p>
                 }
             </React.Fragment>
         );
