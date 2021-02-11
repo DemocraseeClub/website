72a7bc255b80ee77a7b4a060bb3084e9432a3110
Branyer Vergara <Branyervergarac@gmail.com>
2021-02-11 12:25:03 -0400

    Float global note panel #3

--- src/components/AgendaItemTools.js
+++ src/components/AgendaItemTools.js
@@ -5,6 +5,8 @@ import HtmlEditor from "./HtmlEditor";
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
+import '../AgendaItemOverride.css';
+
 class AgendaItemTools extends React.Component {
 
     constructor(p) {
@@ -27,6 +29,23 @@ class AgendaItemTools extends React.Component {
         } else {
             showNotes.push(type);
         }
+        
+        let selected = new Set();
+        document.querySelectorAll('.MuiTabs-flexContainer>.MuiButtonBase-root').forEach((btn, idx) => {
+             btn.style.backgroundColor = "white"
+             showNotes.forEach(n => {
+           
+                if(btn.firstElementChild.firstElementChild.classList.contains(n))
+                    selected.add(btn)
+                 
+             })
+             
+        })
+
+        selected.forEach(btn => {
+            btn.style.backgroundColor = "rgb(199, 199, 199)"
+        })
+
         this.setState({showNotes: showNotes});
     }
 
@@ -35,12 +54,14 @@ class AgendaItemTools extends React.Component {
 
         return (
                         <React.Fragment>
+                       
                             <Tabs
                                 orientation={this.state.showNotes.length > 0 ? "horizontal" : "vertical"}
                                 variant="standard"
                                 id={"noteTabs"}
                                 value={this.state.showNotes}
                                 onChange={(e, index) => {
+                                    
                                     let type = index === 0 ? 'gsheets' : index === 1 ? 'gdocs' : 'private';
                                     this.toggleNotes(type);
                                 }}
@@ -48,34 +69,37 @@ class AgendaItemTools extends React.Component {
                                 className={this.state.showNotes.length > 0 ? classes.tabsHorz : classes.tabsVert}
                                 classes={{flexContainer:classes.spaceAround}}
                             >
-                                <Tab icon={<img alt={'sheets'} src={'/images/Google_Sheets_logo.png'} />} classes={{root:classes.tabsIcon}} />
-                                <Tab icon={<img alt={'docs'} src={'/images/Google_Docs_logo.png'} />} classes={{root:classes.tabsIcon}} />
-                                <Tab icon={<SpeakerNotes/>} classes={{root:classes.tabsIcon}} />
+                                <Tab icon={<img alt={'sheets'} className="gsheets"   src={'/images/Google_Sheets_logo.png'} />}  classes={{root:classes.tabsIcon}} />
+                                <Tab icon={<img alt={'docs'} className="gdocs"  src={'/images/Google_Docs_logo.png'} />}  classes={{root:classes.tabsIcon}} />
+                                <Tab icon={<SpeakerNotes className="private" />}  classes={{root:classes.tabsIcon}} />
                             </Tabs>
+                        
+                            <div style={{maxHeight:'460px', overflowY:'auto',overflowX:'hidden'}}>
+                                <TabPanel value={this.state.showNotes} index={'gsheets'}>
+                                    <iframe
+                                        title='Google Sheets'
+                                        src='https://docs.google.com/spreadsheets/d/1RO1mgOJoZjhGFnh0SnW4ev212LHd3byP1J-zi2BKMp8/edit?usp=sharing&rm=minimal'
+                                        width={'100%'} height={'450'} seamless />
+                                </TabPanel>
+                                <TabPanel value={this.state.showNotes} index={'gdocs'}>
+                                    <iframe
+                                        title='Google Docs'
+                                        src='https://docs.google.com/document/d/1bg5p_GKXJ81CX9YhpvMVepakOcgBYDlLAYaRdFrIV0M/edit?usp=sharing&&rm=minimal'
+                                        width={'100%'} height={'450'} seamless />
+                                </TabPanel>
+                                <TabPanel value={this.state.showNotes} index={'private'}>
+                                    <HtmlEditor
+                                        label="Private Notes"
+                                        toolbar={'static'}
+                                        style={{width: '100%'}}
+                                        variant={'outlined'}
+                                        rows={3}
+                                        html={this.state.notes.private}
+                                        onChange={val => this.handleNote(val, 'private')}
+                                    />
+                                </TabPanel>
 
-                            <TabPanel value={this.state.showNotes} index={'gsheets'}>
-                                <iframe
-                                    title='Google Sheets'
-                                    src='https://docs.google.com/spreadsheets/d/1RO1mgOJoZjhGFnh0SnW4ev212LHd3byP1J-zi2BKMp8/edit?usp=sharing&rm=minimal'
-                                    width={'100%'} height={'450'} seamless />
-                            </TabPanel>
-                            <TabPanel value={this.state.showNotes} index={'gdocs'}>
-                                <iframe
-                                    title='Google Docs'
-                                    src='https://docs.google.com/document/d/1bg5p_GKXJ81CX9YhpvMVepakOcgBYDlLAYaRdFrIV0M/edit?usp=sharing&&rm=minimal'
-                                    width={'100%'} height={'450'} seamless />
-                            </TabPanel>
-                            <TabPanel value={this.state.showNotes} index={'private'}>
-                                <HtmlEditor
-                                    label="Private Notes"
-                                    toolbar={'static'}
-                                    style={{width: '100%'}}
-                                    variant={'outlined'}
-                                    rows={3}
-                                    html={this.state.notes.private}
-                                    onChange={val => this.handleNote(val, 'private')}
-                                />
-                            </TabPanel>
+                            </div>
                         </React.Fragment>
 
         );
