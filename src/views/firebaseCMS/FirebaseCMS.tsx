734148d69698995e747934ea323370ff94e8df56
Branyer Vergara <Branyervergarac@gmail.com>
2021-03-07 11:49:52 -0400

    changing navigation to a diferent file

--- src/views/firebaseCMS/FirebaseCMS.tsx
+++ src/views/firebaseCMS/FirebaseCMS.tsx
@@ -1,26 +1,10 @@
 import React from "react";
 
-import { Authenticator, CMSApp, EntityCollection } from "@camberi/firecms";
+import { Authenticator, CMSApp } from "@camberi/firecms";
 import firebase from "firebase/app";
 import "typeface-rubik";
-
+import navigation from "./navigation";
 import "../../theme/FirebaseCMS.css";
-
-import userCollection from "./collections/user";
-import resourceCollection from "./collections/resource";
-import resourceTypesCollection from "./collections/resource_types";
-import officialCollection from "./collections/official";
-import partyCollection from "./collections/party";
-import cityCollection from "./collections/city";
-import stateCollection from "./collections/state";
-import rallyCollection from "./collections/rally";
-import topicCollection from "./collections/topic";
-import meetingCollection from "./collections/meeting";
-import meetingTypesCollection from "./collections/meeting_types";
-import stakeholderCollection from "./collections/stakeholder";
-import actionPlanCollection from "./collections/action_plan";
-import pageCollection from "./collections/page";
-import inviteCollection from "./collections/invite";
 
 // This is for test
 const firebaseConfig = {
@@ -33,24 +17,6 @@ const firebaseConfig = {
 };
 
 export function FirebaseCMS() {
-  const navigation: EntityCollection[] = [
-    userCollection,
-    resourceCollection,
-    resourceTypesCollection,
-    officialCollection,
-    partyCollection,
-    cityCollection,
-    stateCollection,
-    rallyCollection,
-    topicCollection,
-    meetingCollection,
-    meetingTypesCollection,
-    stakeholderCollection,
-    actionPlanCollection,
-    pageCollection,
-    inviteCollection
-  ];
-
   const myAuthenticator: Authenticator = (user?: firebase.User) => {
     console.log("Allowing access to", user?.email);
     return true;
