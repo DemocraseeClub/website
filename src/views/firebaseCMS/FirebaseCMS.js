685ba49e6c05861a7e49dd1a000ea44464bb0d07
Branyer Vergara <Branyervergarac@gmail.com>
2021-03-09 22:30:21 -0400

    formating code #15

--- src/views/firebaseCMS/FirebaseCMS.js
+++ src/views/firebaseCMS/FirebaseCMS.js
@@ -1,45 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import { Authenticator, CMSApp } from "@camberi/firecms";
-import  firebase from "firebase/app";
+import firebase from "firebase/app";
 import "typeface-rubik";
-import {navigationAdmin, navigationUser} from "./navigation";
+import { navigationAdmin, navigationUser } from "./navigation";
 import "../../theme/FirebaseCMS.css";
 // This is the actual config
 const firebaseConfig = {
   apiKey: "AIzaSyAlMzICClI1d0VPAs5zGmyOO6JEUqLQAic",
-    authDomain: "democraseeclub.firebaseapp.com",
-    databaseURL: "https://democraseeclub.firebaseio.com",
-    projectId: "democraseeclub",
-    storageBucket: "democraseeclub.appspot.com",
-    messagingSenderId: "1051506392090",
-    appId: "1:1051506392090:web:721f69ed2b5afde2a4a5a3",
-    measurementId: "G-XYVYDC8L1N"
+  authDomain: "democraseeclub.firebaseapp.com",
+  databaseURL: "https://democraseeclub.firebaseio.com",
+  projectId: "democraseeclub",
+  storageBucket: "democraseeclub.appspot.com",
+  messagingSenderId: "1051506392090",
+  appId: "1:1051506392090:web:721f69ed2b5afde2a4a5a3",
+  measurementId: "G-XYVYDC8L1N",
 };
 
 export function FirebaseCMS() {
-
-  const [navigation, setNavigation] = useState(navigationUser)
+  const [navigation, setNavigation] = useState(navigationUser);
 
   const myAuthenticator = (user) => {
     console.log("Allowing access to", user?.email);
     //TODO get udsuario de fb
-     window.db.collection('users')
-     .where('email', "==", user.email).get()
-     .then(data => {
-            data.forEach(d => {
-              const user = d.data()
-              const isAdmin = user.roles.some(r => r === "ROLE_ADMIN")
-              if(isAdmin)
-                setNavigation(navigationAdmin)
-
-            }
-          
-            )
-     }
-            )
-     .catch(e => console.log(e))
-     console.log(navigation)
+    window.db
+      .collection("users")
+      .where("email", "==", user.email)
+      .get()
+      .then((data) => {
+        data.forEach((d) => {
+          const user = d.data();
+          const isAdmin = user.roles.some((r) => r === "ROLE_ADMIN");
+          if (isAdmin) setNavigation(navigationAdmin);
+        });
+      })
+      .catch((e) => console.log(e));
+    console.log(navigation);
     return true;
   };
 
