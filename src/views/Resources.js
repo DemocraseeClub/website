d2bcce41e08dea53a90332eef0844963fb39e3ac
Johan Mora <joenmodu99@gmail.com>
2021-03-24 10:27:41 -0400

    Resources blue section added

--- src/views/Resources.js
+++ src/views/Resources.js
@@ -10,6 +10,8 @@ import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import Box from "@material-ui/core/Box";
+import Avatar from '@material-ui/core/Avatar';
+
 
 class Resources extends React.Component {
 
@@ -19,13 +21,43 @@ class Resources extends React.Component {
     }
 
     render() {
+        const {classes} = this.props;
         return (
-            <Box m={4}>
-                <Typography variant={'h4'}>Resources</Typography>
-                <Typography variant={'subtitle1'}>This is a network of supporting organizations and individuals offering
-                    their produces, services or expertise in exchange for <em>CitizenCoin</em> earned through
-                    contributions to this platform</Typography>
-                <Box m={4}>
+            <Box>
+                <Grid container className={classes.blueSection}>
+                    <Grid item xs={8}>
+                        <Typography variant={'h5'} className={classes.blueSectionTitle}><b>Request and Receive Help From Your Community</b></Typography>
+                        <Typography variant={'h6'} className={classes.blueSectionSubtitle}>All action with plans will receive a percentage of the current city pot of <b>2000 Citize Coins</b></Typography>
+                        <Grid container spacing={3} className={classes.blueSectionItemsContainer }>
+                            <Grid item>
+                                <Box display="flex" alignItems="center">
+                                    <Avatar src="/images/democrasee_logo_black.png" className={classes.blueSectionItemImg} alt="citizen-coin"/>
+                                    <Typography variant={'body2'} className={classes.blueSectionItemText}><b>Earn 2 - 2000 Citizen Coins</b></Typography>                                    
+                                </Box>
+                            </Grid>
+                            <Grid item>
+                                <Box display="flex" alignItems="center">
+                                    <Avatar src="/images/democrasee_logo_black.png" className={classes.blueSectionItemImg} alt="hero-badge"/>
+                                    <Typography variant={'body2'} className={classes.blueSectionItemText}><b>Explorer or Hero Badge</b></Typography>
+                                </Box>
+                            </Grid>
+                            <Grid item>
+                                <Box display="flex" alignItems="center">
+                                    <Avatar src="/images/democrasee_logo_black.png" className={classes.blueSectionItemImg} alt="Time-knowledge"/>
+                                    <Typography variant={'body2'} className={classes.blueSectionItemText}><b>Time and Knowledge</b></Typography>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                        <Button variant="outlined" className={classes.blueSectionLeftButton}>Request Help</Button>
+                        <Button className={classes.blueSectionRightButton}>Offer Your Expertise</Button>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <img src="/images/democrasee_logo_black.png" alt="blueSection-hero" className={classes.blueSectionHero}/>
+                        </Box>
+                    </Grid>
+                </Grid>
+                {/* <Box m={4}>
                     <Grid container justify={'space-around'} spacing={4}>
                         <Grid item xs={12} sm={6} md={4}>
                             <Card>
@@ -148,7 +180,7 @@ class Resources extends React.Component {
 
 
                     </Grid>
-                </Box>
+                </Box> */}
             </Box>
         );
     }
@@ -159,6 +191,46 @@ class Resources extends React.Component {
 const useStyles = theme => ({
     root: {
         width: '100%',
+    },
+    blueSection: {
+        background: theme.palette.secondary.main,
+        padding: '20px 70px'
+    },
+    blueSectionTitle: {
+        marginBottom: '10px'
+    },
+    blueSectionSubtitle: {
+        marginBottom: '20px',
+        fontWeight: 'normal',
+        width: '75%'
+    },
+    blueSectionItemText: {
+        width: '50%',
+        color: 'gray'
+    },
+    blueSectionItemImg: {
+        marginRight: '10px'
+    },
+    blueSectionLeftButton: {
+        border: `1px solid ${theme.palette.error.main}`,
+        color: theme.palette.error.main,
+        textTransform: 'none',
+        marginRight: '15px',
+        padding: "5px 10px"
+
+    },
+    blueSectionRightButton: {
+        background: theme.palette.error.main,
+        color: 'white',
+        textTransform: 'none',
+        padding: "5px 20px"
+    },
+    blueSectionHero: {
+        width: "150px",
+        height: "200px"
+    },
+    blueSectionItemsContainer: {
+        marginBottom: '15px'
     }
 });
 
