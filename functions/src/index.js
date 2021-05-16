f2c06a9bb215970dccf79ded58c09aab07ebca9f
Branyer Vergara <Branyervergarac@gmail.com>
2021-05-16 01:02:28 -0400

    start with express app

--- functions/src/index.js
+++ functions/src/index.js
@@ -5,7 +5,19 @@ var path = require('path');
 // const firebase = require('firebase');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express()
+
+
+
+admin.initializeApp({
+    credential: admin.credential.cert("./democraseeclub-firebase-admin.json"),
+    databaseURL: "https://democraseeclub.firebaseio.com"
+});
+
 const db = admin.firestore();
 
 // Retrieve services via the defaultApp variable...
@@ -14,76 +26,83 @@ const db = admin.firestore();
 
 // TODO: replace all with Express app: https://firebase.google.com/docs/functions/http-events
 
-const site_root = path.resolve(__dirname+'/..');
+
+
+
+exports.app = functions.https.onRequest(app);
+
+
+
+// const site_root = path.resolve(__dirname+'/..');
 
 // docs: https://firebase.google.com/docs/reference/functions/providers_https_.request
-exports.injectMeta = functions.https.onRequest((req, res, next) => {
-    const pathname = req.path; // Short-hand for url.parse(req.url).pathname
-    if (pathname.indexOf('/rally/') === 0) {
-        let template = fs.readFileSync(`${site_root}/build/index.html`, 'utf8');
-        console.log("INJECTING ON " + pathname)
-        // TODO: query for rally meta data (description, video, image , ...)
-        let meta = `<meta property="og:description" content="Incentivizing Civic Action" />
-            <meta property="Description" content="Incentivizing Civic Action || Tailgate your townhall" />
-            <meta property="og:url" content="https://democraseeclub.web.app/${pathname}" />`; // strip query params
-            /* meta += `<meta property="fb:app_id" content="267212653413336" />
-            <meta property="og:type" content="music.radio_station" />
-            <meta property="og:title" content="TAM :: Crowdsourced Communal Playlists" />
-            <meta property="og:video" content="https://trackauthoritymusic.com/videos/ftb/mobile/custom/ftb.ts.mp4" />
-            <meta property="og:video:secure_url" content="https://trackauthoritymusic.com/videos/ftb/mobile/custom/ftb.ts.mp4" />
-            <meta property="og:video:type" content="video/mp4" />
-            <meta property="og:video:width" content="360" />
-            <meta property="og:video:height" content="640" />`;
-            */
+// exports.injectMeta = functions.https.onRequest((req, res, next) => {
+//     const pathname = req.path; // Short-hand for url.parse(req.url).pathname
+//     if (pathname.indexOf('/rally/') === 0) {
+//         let template = fs.readFileSync(`${site_root}/build/index.html`, 'utf8');
+//         console.log("INJECTING ON " + pathname)
+//         // TODO: query for rally meta data (description, video, image , ...)
+//         let meta = `<meta property="og:description" content="Incentivizing Civic Action" />
+//             <meta property="Description" content="Incentivizing Civic Action || Tailgate your townhall" />
+//             <meta property="og:url" content="https://democraseeclub.web.app/${pathname}" />`; // strip query params
+//             /* meta += `<meta property="fb:app_id" content="267212653413336" />
+//             <meta property="og:type" content="music.radio_station" />
+//             <meta property="og:title" content="TAM :: Crowdsourced Communal Playlists" />
+//             <meta property="og:video" content="https://trackauthoritymusic.com/videos/ftb/mobile/custom/ftb.ts.mp4" />
+//             <meta property="og:video:secure_url" content="https://trackauthoritymusic.com/videos/ftb/mobile/custom/ftb.ts.mp4" />
+//             <meta property="og:video:type" content="video/mp4" />
+//             <meta property="og:video:width" content="360" />
+//             <meta property="og:video:height" content="640" />`;
+//             */
 
-        template = template.replace("<head>", "<head>" + meta);
-        res.status(200).send(template);
-    } else {
-        next();
-    }
-});
+//         template = template.replace("<head>", "<head>" + meta);
+//         res.status(200).send(template);
+//     } else {
+//         next();
+//     }
+// });
 
-// use FireCMS token to merge POST'd provider with tfirestore doc (or crate new one
-exports.syncUser = functions.https.onRequest((req, res) => {
-    if (!req.body || !req.body.idToken) return false;
-    console.log(req.body);
-    // validate accessToken...
-    return admin
-        .auth()
-        .verifyIdToken(req.body.idToken)
-        .then((decodedToken) => {
-            const uid = decodedToken.uid;
-            let fbUser = getUser(uid);
-            if (!fbUser) {
-                fbUser = {};
-                // TODO: create user
-            }
-            let merged = Object.assign({}, fbUser, req.body.authUser);
-            delete merged.idToken;
-            /* Investigate:
-            auth.currentUser.linkWithRedirect(provider).then().catch();
-            explore: use Firebase Functions to set [Custom Claim](https://firebase.google.com/docs/auth/admin/custom-claims) for faster database Security Rules
-             */
-            console.log("MERGING USER!!", merged);
-            db.doc('users/'+uid).set(merged)
-        })
-        .catch((error) => {
-            return console.error(error);
-        });
-});
+// // use FireCMS token to merge POST'd provider with tfirestore doc (or crate new one
+// exports.syncUser = functions.https.onRequest((req, res) => {
+//     if (!req.body || !req.body.idToken) return false;
+//     console.log(req.body);
+//     validate accessToken...
+//     return admin
+//         .auth()
+//         .verifyIdToken(req.body.idToken)
+//         .then((decodedToken) => {
+//             const uid = decodedToken.uid;
+//             let fbUser = getUser(uid);
+//             if (!fbUser) {
+//                 fbUser = {};
+//                 // TODO: create user
+//             }
+//             let merged = Object.assign({}, fbUser, req.body.authUser);
+//             delete merged.idToken;
+//             /* Investigate:
+//             auth.currentUser.linkWithRedirect(provider).then().catch();
+//             explore: use Firebase Functions to set [Custom Claim](https://firebase.google.com/docs/auth/admin/custom-claims) for faster database Security Rules
+//              */
+//             console.log("MERGING USER!!", merged);
+//             db.doc('users/'+uid).set(merged)
+//         })
+//         .catch((error) => {
+//             return console.error(error);
+//         });
+// });
 
-async function getUser(uid) {
-    // admin.auth.getUserByEmail(),  getUserByPhoneNumber...
-    return await admin.auth().getUser(uid)
-        .then((userRecord) => {
-            console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
-            return userRecord.toJSON();
-        })
-        .catch((error) => {
-            console.error('Error fetching user data:', error);
-            return false;
-        });
-}
+// async function getUser(uid) {
+//     // admin.auth.getUserByEmail(),  getUserByPhoneNumber...
+//     return await admin.auth().getUser(uid)
+//         .then((userRecord) => {
+//             console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+//             return userRecord.toJSON();
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching user data:', error);
+//             return false;
+//         });
+// }
 
 /*
 exports.createUser = functions.firestore
