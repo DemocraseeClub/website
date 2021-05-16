e6f4c6cb8ccba0c42b1bef1085fddf3fb6979b02
eliataylor <eli@taylormadetraffic.com>
2021-05-15 18:09:20 -0700

    cleanup

--- src/views/Resources.js
+++ src/views/Resources.js
@@ -7,11 +7,8 @@ import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import Box from "@material-ui/core/Box";
 import Avatar from '@material-ui/core/Avatar';
-import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import SearchIcon from '@material-ui/icons/Search';
-import InputAdornment from '@material-ui/core/InputAdornment';
 import {rallyStyles} from "../Util/ThemeUtils";
 import Config from "../Config";
 import SanitizedHTML from "react-sanitized-html";
@@ -44,7 +41,7 @@ class Resources extends React.Component {
                 let resourcesDataParsed = [];
                 resourcesData.forEach((doc) => {
                     let resource = {key: doc.id, ...doc.data()};
-
+                    // TODO: replace .image with image uri
                     resourcesDataParsed.push(resource)
                 });
                 return resourcesDataParsed
