50b2dd63fd50ff42122e6e83117895b3bf1a38f2
Branyer Vergara <Branyervergarac@gmail.com>
2021-05-17 00:47:41 -0400

    adding endpoint to get resources and rallies

--- functions/src/index.js
+++ functions/src/index.js
@@ -7,18 +7,29 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
+const { auth } = require('firebase-admin');
+const { bucket } = require('firebase-functions/lib/providers/storage');
 
 
 const app = express()
+
+const urlOptions = {
+    version: "v4",
+    action: "read",
+    expires: Date.now() + 1000 * 60 * 2, // 2 minutes
+}
+
 
 
 
 admin.initializeApp({
     credential: admin.credential.cert("./democraseeclub-firebase-admin.json"),
-    databaseURL: "https://democraseeclub.firebaseio.com"
+    databaseURL: "https://democraseeclub.firebaseio.com",
+    storageBucket: "gs://democraseeclub.appspot.com"
 });
 
 const db = admin.firestore();
+// const storage = admin.storage()
 
 // Retrieve services via the defaultApp variable...
 // var defaultAuth = defaultApp.auth();
@@ -30,11 +41,11 @@ app.get("/user/:uid", async (req, res) => {
 
     try {
 
-        const user =  await admin.auth().getUser(req.params.uid)
+        const user = await admin.auth().getUser(req.params.uid)
 
         return res.status(200).json(user)
 
-    } catch(error) {
+    } catch (error) {
 
         return res.status(500).send(error);
 
@@ -43,11 +54,142 @@ app.get("/user/:uid", async (req, res) => {
 })
 
 
+
+app.get("/resources", async (req, res, next) => {
+
+    try {
+
+        const resourcesSnapshot = await db.collection("resources").get()
+        const { docs } = resourcesSnapshot
+
+        const response = docs.map(doc => (
+            {
+                id: doc.id,
+                ...doc.data()
+            }
+        ))
+
+        for (let i = 0; i < response.length; i++) {
+            if (response[i]?.author) {
+                const author = await response[i].author.get()
+                response[i].author = {id: author.id, ...author.data() }
+            }
+        }
+
+        return res.status(200).json(response)
+
+    } catch (error) {
+
+        return res.status(500).send(error);
+    }
+
+
+
+})
+
+app.get("/rallies", async (req, res, next) => {
+
+    try {
+
+        const ralliesSnapshot = await db.collection("rallies").get()
+        const { docs } = ralliesSnapshot
+
+        const response = docs.map(doc => (
+            {
+                id: doc.id,
+                ...doc.data()
+            }
+        ))
+
+        for (let i = 0; i < response.length; i++) {
+
+            if (response[i]?.author) {
+                const author = await response[i].author.get()
+                response[i].author = {id:author.id, ...author.data() }
+            }
+
+            if (response[i]?.meetings) {
+
+                for (let j = 0; j < response[i].meetings.length; j++) {
+
+                    const meeting = await response[i].meetings[j].get()
+                    response[i].meetings[j] = {id: meeting.id, ...meeting.data() }
+
+                }
+            }
+
+            if (response[i]?.topics) {
+
+                for (let j = 0; j < response[i].topics.length; j++) {
+
+                    const topic = await response[i].topics[j].get()
+                    response[i].topics[j] = {id: topic.id, ...topic.data() }
+
+                }
+            }
+
+            if (response[i]?.stakeholders) {
+
+                for (let j = 0; j < response[i].stakeholders.length; j++) {
+
+                    const stakeholder = await response[i].stakeholders[j].get()
+                    response[i].stakeholders[j] = {id: stakeholder.id, ...stakeholder.data() }
+
+                }
+            }
+
+        }
+
+        return res.status(200).json(response)
+
+    } catch (error) {
+
+        return res.status(500).send(error);
+    }
+})
+
+app.get("/syncUser", (req, res, next) => {
+
+
+
+})
+
+const site_root = path.resolve(__dirname + '/..');
+
+
+app.post("/injectMeta", (req, res, next) => {
+
+    const pathname = req.path; // Short-hand for url.parse(req.url).pathname
+    if (pathname.indexOf('/rally/') === 0) {
+        let template = fs.readFileSync(`${site_root}/build/index.html`, 'utf8');
+        console.log("INJECTING ON " + pathname)
+        // TODO: query for rally meta data (description, video, image , ...)
+        let meta = `<meta property="og:description" content="Incentivizing Civic Action" />
+            <meta property="Description" content="Incentivizing Civic Action || Tailgate your townhall" />
+            <meta property="og:url" content="https://democraseeclub.web.app/${pathname}" />`; // strip query params
+        /* meta += `<meta property="fb:app_id" content="267212653413336" />
+        <meta property="og:type" content="music.radio_station" />
+        <meta property="og:title" content="TAM :: Crowdsourced Communal Playlists" />
+        <meta property="og:video" content="https://trackauthoritymusic.com/videos/ftb/mobile/custom/ftb.ts.mp4" />
+        <meta property="og:video:secure_url" content="https://trackauthoritymusic.com/videos/ftb/mobile/custom/ftb.ts.mp4" />
+        <meta property="og:video:type" content="video/mp4" />
+        <meta property="og:video:width" content="360" />
+        <meta property="og:video:height" content="640" />`;
+        */
+
+        template = template.replace("<head>", "<head>" + meta);
+        res.status(200).send(template);
+    } else {
+        next();
+    }
+
+})
+
 exports.app = functions.https.onRequest(app);
 
 
 
-// const site_root = path.resolve(__dirname+'/..');
+
 
 // docs: https://firebase.google.com/docs/reference/functions/providers_https_.request
 // exports.injectMeta = functions.https.onRequest((req, res, next) => {
@@ -75,6 +217,11 @@ exports.app = functions.https.onRequest(app);
 //         next();
 //     }
 // });
+
+
+
+
+
 
 // // use FireCMS token to merge POST'd provider with tfirestore doc (or crate new one
 // exports.syncUser = functions.https.onRequest((req, res) => {
@@ -111,7 +258,7 @@ exports.app = functions.https.onRequest(app);
 
 // async function getUser(uid) {
     // admin.auth.getUserByEmail(),  getUserByPhoneNumber...
-    
+
 
 /*
 exports.createUser = functions.firestore
