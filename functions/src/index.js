469380490e54a590eeaf3eca6b80e0135af6c7b4
Branyer Vergara <branyervergarac@gmail.com>
2021-05-19 09:09:51 -0400

    adding endpoint to search for rallies with meetings

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,25 +1,23 @@
 // import * as functions from "firebase-functions"
-const fs = require('fs');
-var path = require('path');
+const fs = require("fs");
+var path = require("path");
 
 // const firebase = require('firebase');
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const express = require('express');
-const cors = require('cors');
-const { auth } = require('firebase-admin');
-const { bucket } = require('firebase-functions/lib/providers/storage');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const { auth } = require("firebase-admin");
+const { bucket } = require("firebase-functions/lib/providers/storage");
 
+const app = express();
 
-const app = express()
-
-app.use(cors())
-
+app.use(cors());
 
 admin.initializeApp({
     credential: admin.credential.cert("./democraseeclub-firebase-admin.json"),
     databaseURL: "https://democraseeclub.firebaseio.com",
-    storageBucket: "gs://democraseeclub.appspot.com"
+    storageBucket: "gs://democraseeclub.appspot.com",
 });
 
 const db = admin.firestore();
@@ -32,144 +30,118 @@ const db = admin.firestore();
 // TODO: replace all with Express app: https://firebase.google.com/docs/functions/http-events
 
 app.get("/user/:uid", async (req, res) => {
-
     try {
+        const user = await admin.auth().getUser(req.params.uid);
 
-        const user = await admin.auth().getUser(req.params.uid)
-
-        return res.status(200).json(user)
-
+        return res.status(200).json(user);
     } catch (error) {
-
         return res.status(500).send(error);
-
     }
-
-})
+});
 
 app.get("/users/:role", async (req, res) => {
-
     try {
+        const usersSnapshot = await db
+            .collection("users")
+            .where("roles", "array-contains", req.params.role)
+            .get();
+        const { docs } = usersSnapshot;
 
-        const usersSnapshot = await db.collection("users").where("roles", "array-contains", req.params.role).get()
-        const {docs} = usersSnapshot
+        const response = docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
 
-        const response = docs.map(doc => (
-            {
-                id: doc.id,
-                ...doc.data()
-            }
-        ))
-
-        return res.status(200).json(response)
-
+        return res.status(200).json(response);
     } catch (error) {
-        
         return res.status(500).send(error);
-
     }
-
-
-
-})
-
-
+});
 
 app.get("/resources", async (req, res, next) => {
-
     try {
+        const resourcesSnapshot = await db.collection("resources").get();
+        const { docs } = resourcesSnapshot;
 
-        const resourcesSnapshot = await db.collection("resources").get()
-        const { docs } = resourcesSnapshot
-
-        const response = docs.map(doc => (
-            {
-                id: doc.id,
-                ...doc.data()
-            }
-        ))
+        const response = docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
 
         for (let i = 0; i < response.length; i++) {
             if (response[i]?.author) {
-                const author = await response[i].author.get()
-                response[i].author = {id: author.id, ...author.data() }
+                const author = await response[i].author.get();
+                response[i].author = { id: author.id, ...author.data() };
             }
         }
 
-        return res.status(200).json(response)
-
+        return res.status(200).json(response);
     } catch (error) {
-
         return res.status(500).send(error);
     }
-
-
-
-})
+});
 
 app.get("/rallies", async (req, res, next) => {
-
     try {
+        const ralliesSnapshot = await db.collection("rallies").get();
+        const { docs } = ralliesSnapshot;
 
-        const ralliesSnapshot = await db.collection("rallies").get()
-        const { docs } = ralliesSnapshot
-
-        const response = docs.map(doc => (
-            {
-                id: doc.id,
-                ...doc.data()
-            }
-        ))
+        const response = docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
 
         for (let i = 0; i < response.length; i++) {
-
             if (response[i]?.author) {
-                const author = await response[i].author.get()
-                response[i].author = {id:author.id, ...author.data() }
+                const author = await response[i].author.get();
+                response[i].author = { id: author.id, ...author.data() };
             }
 
             if (response[i]?.meetings) {
-
                 for (let j = 0; j < response[i].meetings.length; j++) {
-
-                    const meeting = await response[i].meetings[j].get()
-                    response[i].meetings[j] = {id: meeting.id, ...meeting.data() }
-
+                    const meeting = await response[i].meetings[j].get();
+                    response[i].meetings[j] = {
+                        id: meeting.id,
+                        ...meeting.data(),
+                    };
                 }
             }
 
             if (response[i]?.topics) {
-
                 for (let j = 0; j < response[i].topics.length; j++) {
-
-                    const topic = await response[i].topics[j].get()
-                    response[i].topics[j] = {id: topic.id, ...topic.data() }
-
+                    const topic = await response[i].topics[j].get();
+                    response[i].topics[j] = { id: topic.id, ...topic.data() };
                 }
             }
 
             if (response[i]?.stakeholders) {
-
                 for (let j = 0; j < response[i].stakeholders.length; j++) {
-
-                    const stakeholder = await response[i].stakeholders[j].get()
-                    response[i].stakeholders[j] = {id: stakeholder.id, ...stakeholder.data() }
-
+                    const stakeholder = await response[i].stakeholders[j].get();
+                    response[i].stakeholders[j] = {
+                        id: stakeholder.id,
+                        ...stakeholder.data(),
+                    };
                 }
             }
 
+            if (response[i]?.wise_demo) {
+                for (let j = 0; j < response[i].wise_demo.length; j++) {
+                    const wise_dem = await response[i].wise_demo[j].get();
+                    response[i].wise_demo[j] = {
+                        id: wise_dem.id,
+                        ...wise_dem.data(),
+                    };
+                }
+            }
         }
 
-        return res.status(200).json(response)
-
+        return res.status(200).json(response);
     } catch (error) {
-
         return res.status(500).send(error);
     }
-})
+});
 
 app.post("/syncUser", async (req, res, next) => {
-
     if (!req.body || !req.body.idToken) return false;
     //     console.log(req.body);
     //     validate accessToken...
@@ -179,8 +151,8 @@ app.post("/syncUser", async (req, res, next) => {
         .then(async (decodedToken) => {
             const uid = decodedToken.uid;
             let snapshot = await db.collection("users").doc(uid).get();
-         
-            let fbUser = {...snapshot.data()}
+
+            let fbUser = { ...snapshot.data() };
 
             if (JSON.stringify(fbUser) === "{}") {
                 fbUser = {
@@ -192,37 +164,98 @@ app.post("/syncUser", async (req, res, next) => {
                     coverPhoto: "",
                     topic_def_json: "",
                     resources: [],
-                    roles: []
+                    roles: [],
                 };
             }
 
-            let merged = Object.assign({}, fbUser, {providerData: req.body.authUser.providerData });
+            let merged = Object.assign({}, fbUser, {
+                providerData: req.body.authUser.providerData,
+            });
             // delete merged.idToken;
             // /* Investigate:
             // auth.currentUser.linkWithRedirect(provider).then().catch();
             // explore: use Firebase Functions to set [Custom Claim](https://firebase.google.com/docs/auth/admin/custom-claims) for faster database Security Rules
             //     */
             console.log("MERGING USER!!", merged);
-            db.collection('users').doc(uid).set(merged)
+            db.collection("users").doc(uid).set(merged);
 
-            return res.status(200).json(merged)
-
+            return res.status(200).json(merged);
         })
         .catch((error) => {
             return res.status(500).send(error);
         });
+});
 
-})
+app.get("/ralliesWithUpcomingMeetings", async (req, res) => {
+    try {
+        const ralliesSnapshot = await db
+            .collection("rallies")
+            .where("meetings", "!=", [])
+            .get();
+        const { docs } = ralliesSnapshot;
 
-const site_root = path.resolve(__dirname + '/..');
+        const response = docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
 
+        for (let i = 0; i < response.length; i++) {
+            if (response[i]?.author) {
+                const author = await response[i].author.get();
+                response[i].author = { id: author.id, ...author.data() };
+            }
+
+            if (response[i]?.meetings) {
+                for (let j = 0; j < response[i].meetings.length; j++) {
+                    const meeting = await response[i].meetings[j].get();
+                    response[i].meetings[j] = {
+                        id: meeting.id,
+                        ...meeting.data(),
+                    };
+                }
+            }
+
+            if (response[i]?.topics) {
+                for (let j = 0; j < response[i].topics.length; j++) {
+                    const topic = await response[i].topics[j].get();
+                    response[i].topics[j] = { id: topic.id, ...topic.data() };
+                }
+            }
+
+            if (response[i]?.stakeholders) {
+                for (let j = 0; j < response[i].stakeholders.length; j++) {
+                    const stakeholder = await response[i].stakeholders[j].get();
+                    response[i].stakeholders[j] = {
+                        id: stakeholder.id,
+                        ...stakeholder.data(),
+                    };
+                }
+            }
+        }
+
+        if (response[i]?.wise_demo) {
+            for (let j = 0; j < response[i].wise_demo.length; j++) {
+                const wise_dem = await response[i].wise_demo[j].get();
+                response[i].wise_demo[j] = {
+                    id: wise_dem.id,
+                    ...wise_dem.data(),
+                };
+            }
+        }
+
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+const site_root = path.resolve(__dirname + "/..");
 
 app.post("/injectMeta", (req, res, next) => {
-
     const pathname = req.path; // Short-hand for url.parse(req.url).pathname
-    if (pathname.indexOf('/rally/') === 0) {
-        let template = fs.readFileSync(`${site_root}/build/index.html`, 'utf8');
-        console.log("INJECTING ON " + pathname)
+    if (pathname.indexOf("/rally/") === 0) {
+        let template = fs.readFileSync(`${site_root}/build/index.html`, "utf8");
+        console.log("INJECTING ON " + pathname);
         // TODO: query for rally meta data (description, video, image , ...)
         let meta = `<meta property="og:description" content="Incentivizing Civic Action" />
             <meta property="Description" content="Incentivizing Civic Action || Tailgate your townhall" />
@@ -242,76 +275,38 @@ app.post("/injectMeta", (req, res, next) => {
     } else {
         next();
     }
-
-})
+});
 
 exports.app = functions.https.onRequest(app);
 
-
-
-// // use FireCMS token to merge POST'd provider with tfirestore doc (or crate new one
-// exports.syncUser = functions.https.onRequest((req, res) => {
-//     if (!req.body || !req.body.idToken) return false;
-//     console.log(req.body);
-//     validate accessToken...
-//     return admin
-//         .auth()
-//         .verifyIdToken(req.body.idToken)
-//         .then((decodedToken) => {
-//             const uid = decodedToken.uid;
-//            
-//             if (!fbUser) {
-//                 fbUser = {};
-//                 // TODO: create user
-//             }
-//             let merged = Object.assign({}, fbUser, req.body.authUser);
-//             delete merged.idToken;
-//             /* Investigate:
-//             auth.currentUser.linkWithRedirect(provider).then().catch();
-//             explore: use Firebase Functions to set [Custom Claim](https://firebase.google.com/docs/auth/admin/custom-claims) for faster database Security Rules
-//              */
-//             console.log("MERGING USER!!", merged);
-//             db.doc('users/'+uid).set(merged)
-//         })
-//         .catch((error) => {
-//             return console.error(error);
-//         });
-// });
-
-
-
-/*
 exports.createUser = functions.firestore
-    .document('users/{userId}')
+    .document("users/{userId}")
     .onCreate((snap, context) => {
         let uid = context.auth?.uid || context.params.userId;
         const authUser = snap.data();
         const fbUser = getUser(uid);
         let merged = Object.assign({}, fbUser, authUser);
-        TODO: auto award promo citizencoin?
+        //TODO: auto award promo citizencoin?
         console.log("CREATING MERGED USER!!", context, merged);
-        db.doc('users/'+uid).set(merged)
+        db.doc("users/" + uid).set(merged);
     });
 
 exports.updateUser = functions.firestore
-    .document('users/{userId}')
+    .document("users/{userId}")
     .onUpdate((change, context) => {
         // TODO: verify no auth fields / scopes / permissions have changed
     });
 
 exports.deleteUser = functions.firestore
-    .document('users/{userId}')
+    .document("users/{userId}")
     .onDelete((change, context) => {
         // TODO cleanup orphan records
     });
-*/
 
-/*
-https://firebase.google.com/docs/auth/extend-with-functions
+//https://firebase.google.com/docs/auth/extend-with-functions
 exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
     // ...
 });
 exports.sendByeEmail = functions.auth.user().onDelete((user) => {
-  // ...
+    // ...
 });
-*/
