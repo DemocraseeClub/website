b15ce515f4d39f90186698d627dec5ef0ffce7bd
Branyer Vergara <Branyervergarac@gmail.com>
2021-06-20 13:58:54 -0400

    adding userContext file

---
+++ src/contexts/userContext.js
@@ -0,0 +1,23 @@
+import React, {createContext, useState} from "react"
+
+
+
+let initialData = {  user: null, setUser: (newData) => {}}
+
+const UserContext = createContext(initialData)
+
+export default UserContext
+
+
+
+export function UserContextProvider({children}) {
+
+    const [user, setUser] = useState(null)
+
+    return (
+        <UserContext.Provider value={{user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    )
+
+}
